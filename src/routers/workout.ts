29a94dac57837403b7902267f6f87e7b0bf06bfb
Rob Monday <10903531+robmonday@users.noreply.github.com>
2023-03-28 17:46:11 -0400

    added more panels, refined select workout feature

--- src/routers/workout.ts
+++ src/routers/workout.ts
@@ -2,36 +2,59 @@ import app, { Request, Response } from "express";
 import db, { knownDbError } from "../db";
 const workoutRouter = app.Router();
 
+const asyncErrorHandler = (e: any, res: Response) => {
+  console.error(e);
+  if (e instanceof knownDbError && e.code === "P2003") {
+    res.status(409).json({
+      message: "Foreign key error",
+    });
+  } else if (e instanceof Error) {
+    res.status(400).json(e);
+  }
+};
+
 workoutRouter.get("/", async (req: Request, res: Response) => {
-  const workouts = await db.workout.findMany({
-    include: {
-      workoutType: { select: { name: true } },
-    },
-  });
-  res.json(workouts);
+  try {
+    const workouts = await db.workout.findMany({
+      include: {
+        workoutType: { select: { name: true } },
+      },
+    });
+    res.json(workouts);
+  } catch (e) {
+    asyncErrorHandler(e, res);
+  }
 });
 
 workoutRouter.get("/type", async (req: Request, res: Response) => {
-  const workouts = await db.workoutType.findMany({
-    select: { id: true, name: true },
-  });
-  res.json(workouts);
+  try {
+    const workouts = await db.workoutType.findMany({
+      select: { id: true, name: true },
+    });
+    res.json(workouts);
+  } catch (e) {
+    asyncErrorHandler(e, res);
+  }
 });
 
 workoutRouter.post("/", async (req: Request, res: Response) => {
-  const user = await db.user.findFirst({});
-  if (!user) {
-    res.json({
-      message: "unable to create workout because unable to identify user",
+  try {
+    const user = await db.user.findFirst({});
+    if (!user) {
+      res.json({
+        message: "unable to create workout because unable to identify user",
+      });
+    }
+    const workout = await db.workout.create({
+      data: { ...req.body, userId: user?.id },
+      include: {
+        workoutType: { select: { name: true } },
+      },
     });
+    res.json(workout);
+  } catch (e) {
+    asyncErrorHandler(e, res);
   }
-  const workout = await db.workout.create({
-    data: { ...req.body, userId: user?.id },
-    include: {
-      workoutType: { select: { name: true } },
-    },
-  });
-  res.json(workout);
 });
 
 workoutRouter.put("/", async (req: Request, res: Response) => {
@@ -53,25 +76,22 @@ workoutRouter.put("/", async (req: Request, res: Response) => {
     });
     res.json(workout);
   } catch (e) {
-    console.error(e);
-    if (e instanceof knownDbError && e.code === "P2003") {
-      res.status(409).json({
-        message: "Foreign key error",
-      });
-    } else if (e instanceof Error) {
-      res.status(400).json(e);
-    }
+    asyncErrorHandler(e, res);
   }
 });
 
 workoutRouter.delete("/", async (req: Request, res: Response) => {
-  const workout = await db.workout.delete({
-    where: { id: req.body.id },
-    include: {
-      workoutType: { select: { name: true } },
-    },
-  });
-  res.json(workout);
+  try {
+    const workout = await db.workout.delete({
+      where: { id: req.body.id },
+      include: {
+        workoutType: { select: { name: true } },
+      },
+    });
+    res.json(workout);
+  } catch (e) {
+    asyncErrorHandler(e, res);
+  }
 });
 
 export default workoutRouter;
