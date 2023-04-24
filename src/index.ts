161da5441bd538e5474fcc928873c28bdb571998
Rob Monday <10903531+robmonday@users.noreply.github.com>
2023-04-24 15:53:04 -0400

    deploy update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import app from "./server";
 
-const PORT = process.env.PORT || 5174;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Now listening on http://localhost:${PORT}`);
